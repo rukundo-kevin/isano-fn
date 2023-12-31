fdef7f079747847005a5774fa1d9b8f9292a93c1
Rukundo Kevin <38954786+rukundo-kevin@users.noreply.github.com>
2023-12-31 17:05:07 +0200

    Update FamilyTree.tsx

--- src/pages/FamilyTree.tsx
+++ src/pages/FamilyTree.tsx
@@ -176,7 +176,7 @@ export default function FamilyTree() {
       <Tree
         data={familyTree}
         pathFunc={drawStepPath}
-        orientation="vertical"
+        orientation="horizontal"
         translate={translate}
         dimensions={dimensions}
         centeringTransitionDuration={1000}
